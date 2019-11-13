daa9b80fabc15d7158cbbc76c6addf93e33b1f8f
david <up201706766@fe.up.pt>
2019-11-13 11:50:05 +0000

    path

--- TP2/squex.pl
+++ TP2/squex.pl
@@ -174,3 +174,16 @@ floodWhite(Row, Col):-
 
 	
 
+path(A,B) :-   % two nodes are connected, if
+  walk(A,B,[]) % - if we can walk from one to the other,
+  .            % first seeding the visited list with the empty list
+
+walk(A,B,V) :-       % we can walk from A to B...
+  edge(A,X) ,        % - if A is connected to X, and
+  \+(member(X,V)) , % - we haven't yet visited X, and
+  (                  % - either
+    B = X            %   - X is the desired destination
+  ;                  %   OR
+    walk(X,B,[A|V])  %   - we can get to it from X
+  )                  %
+  .                  % Easy!
