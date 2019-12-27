63a80292cd161b48ca6d339b277f28a245a8654f
José Gomes <zemiguelgomes99@gmail.com>
2019-12-27 16:29:36 -0100

    Display complete

--- TP3 - Restrições/aztec.pl
+++ TP3 - Restrições/aztec.pl
@@ -1,7 +1,5 @@
 :- use_module(library(clpfd)).
 
-puzzle(1, [[2], [_, _], [7, _, 6], [_, _, _, _], [4, _, 5, _, 3]]).
-
 appendlist([], Final, Final).
 
 appendlist([H|T], List, Final) :-
@@ -24,27 +22,33 @@ constraints_list([], List1).
 constraints_list([H|List1], [H1 | [H2 | List2]]) :-
     H in 1..9, H1 in 1..9, H2 in 1..9,
     all_different([H|List1]),
-    all_different([H1 | [H2 | List2]]),    
-    math_constraint(H, H1, H2),
+    all_different([H1 | [H2 | List2]]),
+    (H #= H1 + H2 #\/ %SOMA
+    H #= abs(H1 - H2) #\/ %SUBTRAÇÃO
+    H #= H1 * H2 #\/ % MULTIPLICAÇÃO
+    ( H1 #> H2 #/\ 0 #= mod(H1, H2) #/\ H #= H1 / H2) #\/ % DIVISÂO LEFT RIGHT
+    ( H1 #< H2 #/\ 0 #= mod(H2, H1) #/\ H #= H2 / H1)), % DIVISÃO RIGHt LEFT
     constraints_list(List1, [H2 | List2]).
 
-math_constraint(H, H1, H2) :-
-    H #= H1 + H2.
+puzzle(1, [[4], [_, _], [8, _, 3], [_, _, _, _], [1, _, 5, _, 6]]).
 
-math_constraint(H, H1, H2) :-
-    H #= abs(H1 - H2).
+nTabs(0).
+nTabs(X):-
+    write(' '),
+    X1 is X-1,
+    nTabs(X1).
 
-math_constraint(H, H1, H2) :-
-    H #= H1 * H2.
+displayPuzzle(List):-
+    length(List, Size),
+    NewSize is Size -1,!,
+    displayPuzzleAux(List, NewSize).
+    
 
-math_constraint(H, H1, H2) :-
-    H1 #> H2,
-    0 #= mod(H1, H2),
-    H #= H1 / H2.
+displayPuzzleAux([],_):-
+    nl.
 
-math_constraint(H, H1, H2) :-
-    H1 #< H2,
-    0 #= mod(H2, H1),
-    H #= H2 / H1.
-
-
+displayPuzzleAux([Line| List], Size):-
+    nTabs(Size),
+    write(Line), nl,
+    NewSize is Size -1, !,
+    displayPuzzleAux(List, NewSize).
